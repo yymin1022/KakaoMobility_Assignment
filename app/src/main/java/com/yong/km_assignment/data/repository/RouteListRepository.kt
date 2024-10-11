5b4b781d697833a4d314e988a002ec259aa551c2
yymin1022 <yymin1022@gmail.com>
2024-10-11 17:19:14 +0900

    Update RouteListRepository : Removed unnecessary Interface Definition

--- app/src/main/java/com/yong/km_assignment/data/repository/RouteListRepository.kt
+++ app/src/main/java/com/yong/km_assignment/data/repository/RouteListRepository.kt
@@ -5,13 +5,9 @@ import com.yong.km_assignment.data.api.RouteApi
 import com.yong.km_assignment.data.model.RouteList
 import com.yong.km_assignment.util.ApiUtil
 
-interface RouteListRepository {
-    suspend fun getRouteList(): RouteList
-}
-
-class DefaultRouteListRepository: RouteListRepository {
+class RouteListRepository {
     private val api: RouteApi = ApiUtil.getRouteApi()
-    override suspend fun getRouteList(): RouteList {
+    suspend fun getRouteList(): RouteList {
         api.getRouteList().let {
             Log.d("RouteList Repository", it.body().toString())
             return it.body() as RouteList
