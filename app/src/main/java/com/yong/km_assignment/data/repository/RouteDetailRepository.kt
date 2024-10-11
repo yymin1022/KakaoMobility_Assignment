ed932b4c8c8511fb557bd6349c2e221f70715ad0
yymin1022 <yymin1022@gmail.com>
2024-10-11 23:16:32 +0900

    Update RouteDetailRepository : Added API Call

--- app/src/main/java/com/yong/km_assignment/data/repository/RouteDetailRepository.kt
+++ app/src/main/java/com/yong/km_assignment/data/repository/RouteDetailRepository.kt
@@ -1,4 +1,14 @@
 package com.yong.km_assignment.data.repository
 
+import com.yong.km_assignment.data.api.RouteApi
+import com.yong.km_assignment.data.model.RouteDetail
+import com.yong.km_assignment.util.ApiUtil
+
 class RouteDetailRepository {
+    private val api: RouteApi = ApiUtil.getRouteApi()
+    suspend fun getRouteDetail(routeFrom: String, routeTo: String): List<RouteDetail> {
+        api.getRouteDetail(routeFrom, routeTo).let {
+            return it.body() as List<RouteDetail>
+        }
+    }
 }
