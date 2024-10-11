8ed2031c1f9b8a10e34d04007f7de5209ac3bbdf
yymin1022 <yymin1022@gmail.com>
2024-10-12 03:42:14 +0900

    Update RouteInfoRepository : Added Retrofit Call

--- app/src/main/java/com/yong/km_assignment/data/repository/RouteInfoRepository.kt
+++ app/src/main/java/com/yong/km_assignment/data/repository/RouteInfoRepository.kt
@@ -1,4 +1,14 @@
 package com.yong.km_assignment.data.repository
 
+import com.yong.km_assignment.data.api.RouteApi
+import com.yong.km_assignment.data.model.RouteInfo
+import com.yong.km_assignment.util.ApiUtil
+
 class RouteInfoRepository {
+    private val api: RouteApi = ApiUtil.getRouteApi()
+    suspend fun getRouteInfo(routeFrom: String, routeTo: String): RouteInfo? {
+        api.getRouteInfo(routeFrom, routeTo).let { apiResult ->
+            return apiResult.body()
+        }
+    }
 }
