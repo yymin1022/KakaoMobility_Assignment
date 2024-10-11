6a7471b606d88ff56427fae102a4ced7e6f67db7
yymin1022 <yymin1022@gmail.com>
2024-10-11 16:23:01 +0900

    Update RouteListRepository : Added Basic Logic

--- app/src/main/java/com/yong/km_assignment/data/repository/RouteListRepository.kt
+++ app/src/main/java/com/yong/km_assignment/data/repository/RouteListRepository.kt
@@ -1,4 +1,14 @@
 package com.yong.km_assignment.data.repository
 
-class RouteListRepository {
+import com.yong.km_assignment.data.model.ApiResult
+import com.yong.km_assignment.data.model.RouteList
+
+interface RouteListRepository {
+    fun getRouteList(): RouteList
+}
+
+class DefaultRouteListRepository: RouteListRepository {
+    override fun getRouteList(): RouteList {
+        return RouteList(ApiResult.API_OK, listOf())
+    }
 }
