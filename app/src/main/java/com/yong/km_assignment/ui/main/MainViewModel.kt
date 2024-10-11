187b3bab93558784ec697be610b449b0b8f11ceb
yymin1022 <yymin1022@gmail.com>
2024-10-11 16:23:27 +0900

    Update MainViewModel : Added Basic Logic

--- app/src/main/java/com/yong/km_assignment/ui/main/MainViewModel.kt
+++ app/src/main/java/com/yong/km_assignment/ui/main/MainViewModel.kt
@@ -1,12 +1,29 @@
 package com.yong.km_assignment.ui.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.yong.km_assignment.data.model.RouteList
+import com.yong.km_assignment.data.repository.DefaultRouteListRepository
 import com.yong.km_assignment.data.repository.RouteListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val repository: RouteListRepository
+    private val repository: RouteListRepository = DefaultRouteListRepository()
 ): ViewModel() {
+    private val _routeList: MutableLiveData<RouteList> = MutableLiveData()
+    val routeList: LiveData<RouteList> = _routeList
+
+    fun getRouteList() {
+        repository.getRouteList().let {
+            _routeList.postValue(it)
+        }
+    }
+
+    fun onItemClick(position: Int) {
+        Log.d("RouteList", "${routeList.value?.routeList?.get(position)}")
+    }
 }
