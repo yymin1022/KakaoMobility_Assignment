0d13a3db0cb0a2bbd8018854b8a71b52d3a5f781
yymin1022 <yymin1022@gmail.com>
2024-10-12 01:03:18 +0900

    Update MapviewActivity : Added KakaoMap View

--- app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewActivity.kt
+++ app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewActivity.kt
@@ -1,6 +1,7 @@
 package com.yong.km_assignment.ui.mapview
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -8,7 +9,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
+import java.lang.Exception
 
 class MapviewActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +29,24 @@ class MapviewActivity : ComponentActivity() {
 fun KakaoMapView(modifier: Modifier = Modifier) {
     AndroidView(
         factory = { context ->
-            MapView(context).apply {
+            val mapView = MapView(context)
+            mapView.start(
+                object: MapLifeCycleCallback() {
+                    override fun onMapDestroy() {
+                        Log.d("KakaoMapView", "Map Destroyed")
+                    }
 
-            }
+                    override fun onMapError(error: Exception) {
+                        Log.e("KakaoMapView", "Map Error: ${error.message}")
+                    }
+                },
+                object: KakaoMapReadyCallback() {
+                    override fun onMapReady(kakaoMap: KakaoMap) {
+                        Log.d("KakaoMapView", "Map is Ready")
+                    }
+                }
+            )
+            mapView
         },
         modifier = modifier.fillMaxSize()
     )
