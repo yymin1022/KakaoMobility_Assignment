a8d909fea346bad63f64122de4169e26fb228cce
yymin1022 <yymin1022@gmail.com>
2024-10-11 15:31:17 +0900

    Added MainViewModel

---
+++ app/src/main/java/com/yong/km_assignment/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.yong.km_assignment.ui.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewModel: ViewModel() {
+}
