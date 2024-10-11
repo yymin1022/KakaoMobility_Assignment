bb83e6f8806702105649c8fe51fb81ef6d1d7518
yymin1022 <yymin1022@gmail.com>
2024-10-12 02:42:22 +0900

    Update MainActivity : Cleaned UP

--- app/src/main/java/com/yong/km_assignment/ui/main/MainActivity.kt
+++ app/src/main/java/com/yong/km_assignment/ui/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.yong.km_assignment.ui.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
