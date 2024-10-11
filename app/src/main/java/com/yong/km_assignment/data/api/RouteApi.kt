4e12ed60a731b21a84990e270d84ef81d28fe5d5
yymin1022 <yymin1022@gmail.com>
2024-10-11 14:29:41 +0900

    Update RouteApi : Defined HTTP Headers

--- app/src/main/java/com/yong/km_assignment/data/api/RouteApi.kt
+++ app/src/main/java/com/yong/km_assignment/data/api/RouteApi.kt
@@ -5,21 +5,29 @@ import com.yong.km_assignment.data.model.RouteInfo
 import com.yong.km_assignment.data.model.RouteList
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface RouteApi {
+    @Headers(
+        "Authorization: API_KEY",
+        "Content-Type: application/json")
     @GET("")
     suspend fun getRouteDetail(
         @Query("origin") origin: String,
         @Query("destination") destination: String
     ): Response<RouteDetail>
 
+    @Headers(
+        "Authorization: API_KEY",
+        "Content-Type: application/json")
     @GET("")
     suspend fun getRouteInfo(
         @Query("origin") origin: String,
         @Query("destination") destination: String
     ): Response<RouteInfo>
 
+    @Headers("Authorization: API_KEY")
     @GET("")
     suspend fun getRouteList(): Response<RouteList>
 }
