5e2ea8ba08fa8d682db96c43449b2e597340e623
yymin1022 <yymin1022@gmail.com>
2024-10-13 02:52:35 +0900

    Update MapviewViewModel : Added RouteDetail Logic

--- app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewViewModel.kt
+++ app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yong.km_assignment.data.model.RouteDetail
-import com.yong.km_assignment.data.model.RouteListItem
 import com.yong.km_assignment.data.repository.RouteDetailRepository
 import kotlinx.coroutines.launch
 
@@ -14,10 +13,9 @@ class MapviewViewModel: ViewModel() {
     private val _routeDetail: MutableLiveData<List<RouteDetail>?> = MutableLiveData()
     val routeDetail: LiveData<List<RouteDetail>?> = _routeDetail
 
-    fun getRouteDetail(route: RouteListItem) {
-        _routeDetail.postValue(null)
+    fun getRouteDetail(routeFrom: String, routeTo: String) {
         viewModelScope.launch {
-            _repositoryDetail.getRouteDetail(route.routeFrom, route.routeTo).let {
+            _repositoryDetail.getRouteDetail(routeFrom, routeTo).let {
                 _routeDetail.postValue(it)
             }
         }
