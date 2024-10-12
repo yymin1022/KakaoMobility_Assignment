9acc188273523f313bdb6e1280d34756c4844dbe
yymin1022 <yymin1022@gmail.com>
2024-10-13 02:51:16 +0900

    Update MainViewModel : Removed RouteDetail Logic

--- app/src/main/java/com/yong/km_assignment/ui/main/MainViewModel.kt
+++ app/src/main/java/com/yong/km_assignment/ui/main/MainViewModel.kt
@@ -4,19 +4,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.yong.km_assignment.data.model.RouteDetail
 import com.yong.km_assignment.data.model.RouteList
-import com.yong.km_assignment.data.model.RouteListItem
-import com.yong.km_assignment.data.repository.RouteDetailRepository
 import com.yong.km_assignment.data.repository.RouteListRepository
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
-    private val _repositoryDetail = RouteDetailRepository()
     private val _repositoryList = RouteListRepository()
-    private val _routeDetail: MutableLiveData<List<RouteDetail>?> = MutableLiveData()
     private val _routeList: MutableLiveData<RouteList> = MutableLiveData()
-    val routeDetail: LiveData<List<RouteDetail>?> = _routeDetail
     val routeList: LiveData<RouteList> = _routeList
 
     fun getRouteList() {
@@ -25,15 +19,6 @@ class MainViewModel: ViewModel() {
                 if(it != null) {
                     _routeList.postValue(it)
                 }
-            }
-        }
-    }
-
-    fun getRouteDetail(route: RouteListItem) {
-        _routeDetail.postValue(null)
-        viewModelScope.launch {
-            _repositoryDetail.getRouteDetail(route.routeFrom, route.routeTo).let {
-                _routeDetail.postValue(it)
             }
         }
     }
