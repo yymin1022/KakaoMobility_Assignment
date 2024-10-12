78e41487b0b4b6d5bff97e1480f225adcbfcb06a
yymin1022 <yymin1022@gmail.com>
2024-10-13 02:40:42 +0900

    Update MapviewViewModel : Added RotueDetail API Logic

--- app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewViewModel.kt
+++ app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewViewModel.kt
@@ -3,11 +3,25 @@ package com.yong.km_assignment.ui.mapview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.yong.km_assignment.data.model.RouteDetail
+import com.yong.km_assignment.data.model.RouteListItem
+import com.yong.km_assignment.data.repository.RouteDetailRepository
+import kotlinx.coroutines.launch
 
 class MapviewViewModel: ViewModel() {
-    private val _routeDetail: MutableLiveData<List<RouteDetail>> = MutableLiveData()
-    val routeDetail: LiveData<List<RouteDetail>> = _routeDetail
+    private val _repositoryDetail = RouteDetailRepository()
+    private val _routeDetail: MutableLiveData<List<RouteDetail>?> = MutableLiveData()
+    val routeDetail: LiveData<List<RouteDetail>?> = _routeDetail
+
+    fun getRouteDetail(route: RouteListItem) {
+        _routeDetail.postValue(null)
+        viewModelScope.launch {
+            _repositoryDetail.getRouteDetail(route.routeFrom, route.routeTo).let {
+                _routeDetail.postValue(it)
+            }
+        }
+    }
 
     fun setRouteDetail(routeDetail: List<RouteDetail>) {
         _routeDetail.postValue(routeDetail)
