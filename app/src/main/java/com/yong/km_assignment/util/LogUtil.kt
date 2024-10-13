85c6d29a6b2352d0879db92f86195afb510a89ed
yymin1022 <yymin1022@gmail.com>
2024-10-13 23:02:31 +0900

    Update LogUtil : Added Log Functions

--- app/src/main/java/com/yong/km_assignment/util/LogUtil.kt
+++ app/src/main/java/com/yong/km_assignment/util/LogUtil.kt
@@ -1,5 +1,29 @@
 package com.yong.km_assignment.util
 
+import android.util.Log
+
 object LogUtil {
-    
+    fun LogD(msg: String) {
+        Log.d("KM Assignment", msg)
+    }
+
+    fun LogD(tag: String, msg: String) {
+        Log.d(tag, msg)
+    }
+
+    fun LogE(msg: String) {
+        Log.e("KM Assignment", msg)
+    }
+
+    fun LogE(tag: String, msg: String) {
+        Log.e(tag, msg)
+    }
+
+    fun LogI(msg: String) {
+        Log.i("KM Assignment", msg)
+    }
+
+    fun LogI(tag: String, msg: String) {
+        Log.i(tag, msg)
+    }
 }
