7a4bc2448a9828565d411c34ae14909f82902231
yymin1022 <yymin1022@gmail.com>
2024-10-14 15:40:51 +0900

    Update MapviewActivity : Seperated RouteInfo Composable

--- app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewActivity.kt
+++ app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewActivity.kt
@@ -6,20 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.yong.km_assignment.data.model.RouteInfo
 import com.yong.km_assignment.ui.common.StatusTextView
 import com.yong.km_assignment.ui.theme.KakaoMobilityAssignmentTheme
 import com.yong.km_assignment.util.NetworkUtil
@@ -94,31 +89,6 @@ class MapviewActivity: ComponentActivity() {
                     }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun RouteInfoView(
-    routeInfo: RouteInfo?
-) {
-    Column {
-        if (routeInfo != null) {
-            Text(
-                modifier = Modifier.padding(start = 20.dp, top = 20.dp),
-                fontSize = 20.sp,
-                text = "시간: %d시간 %d분".format(routeInfo.routeTime / 3600, (routeInfo.routeTime % 3600) / 60)
-            )
-            Text(
-                modifier = Modifier.padding(start = 20.dp, top = 10.dp, bottom = 20.dp),
-                text = "거리: %,.1fkm".format(routeInfo.routeDistance / 1000f),
-                fontSize = 20.sp
-            )
-        } else {
-            StatusTextView(
-                modifier = Modifier.padding(all = 20.dp),
-                msg = "경로 시간/거리 정보를 불러오지 못했습니다.",
-            )
         }
     }
 }
--- app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewRouteInfo.kt
+++ app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewRouteInfo.kt
@@ -1,1 +1,36 @@
 package com.yong.km_assignment.ui.mapview
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yong.km_assignment.data.model.RouteInfo
+import com.yong.km_assignment.ui.common.StatusTextView
+
+@Composable
+fun RouteInfoView(
+    routeInfo: RouteInfo?
+) {
+    Column {
+        if (routeInfo != null) {
+            Text(
+                modifier = Modifier.padding(start = 20.dp, top = 20.dp),
+                fontSize = 20.sp,
+                text = "시간: %d시간 %d분".format(routeInfo.routeTime / 3600, (routeInfo.routeTime % 3600) / 60)
+            )
+            Text(
+                modifier = Modifier.padding(start = 20.dp, top = 10.dp, bottom = 20.dp),
+                text = "거리: %,.1fkm".format(routeInfo.routeDistance / 1000f),
+                fontSize = 20.sp
+            )
+        } else {
+            StatusTextView(
+                modifier = Modifier.padding(all = 20.dp),
+                msg = "경로 시간/거리 정보를 불러오지 못했습니다.",
+            )
+        }
+    }
+}
