d9f07d0c5f79465d024932d510e88f234fa595a6
yymin1022 <yymin1022@gmail.com>
2024-10-14 16:05:25 +0900

    Update RouteDetail Model : Disabled Parcelable

--- app/src/main/java/com/yong/km_assignment/data/model/RouteDetail.kt
+++ app/src/main/java/com/yong/km_assignment/data/model/RouteDetail.kt
@@ -1,7 +1,5 @@
 package com.yong.km_assignment.data.model
 
-import android.os.Parcel
-import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class RouteDetail(
@@ -9,28 +7,4 @@ data class RouteDetail(
     val routePointList: String,
     @SerializedName("traffic_state")
     val routeTraffic: String
-): Parcelable {
-    constructor(parcel: Parcel): this(
-        parcel.readString() ?: "",
-        parcel.readString() ?: ""
-    )
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(routePointList)
-        dest.writeString(routeTraffic)
-    }
-
-    companion object CREATOR : Parcelable.Creator<RouteDetail> {
-        override fun createFromParcel(parcel: Parcel): RouteDetail {
-            return RouteDetail(parcel)
-        }
-
-        override fun newArray(size: Int): Array<RouteDetail?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
