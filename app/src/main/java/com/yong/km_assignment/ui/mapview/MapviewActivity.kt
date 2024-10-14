99c11eadcb2034661e913714d5f9f7c1289d9256
yymin1022 <yymin1022@gmail.com>
2024-10-14 15:39:34 +0900

    Update MapviewActivity : Seperated KakapMap Composable

--- app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewActivity.kt
+++ app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewActivity.kt
@@ -17,34 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
-import com.kakao.vectormap.KakaoMap
-import com.kakao.vectormap.KakaoMapReadyCallback
-import com.kakao.vectormap.LatLng
-import com.kakao.vectormap.MapLifeCycleCallback
-import com.kakao.vectormap.MapView
-import com.kakao.vectormap.camera.CameraAnimation
-import com.kakao.vectormap.camera.CameraUpdateFactory.newCenterPosition
-import com.kakao.vectormap.camera.CameraUpdateFactory.zoomTo
-import com.kakao.vectormap.label.LabelOptions
-import com.kakao.vectormap.label.LabelStyle
-import com.kakao.vectormap.label.LabelStyles
-import com.kakao.vectormap.label.LabelTextBuilder
-import com.kakao.vectormap.route.RouteLineOptions
-import com.kakao.vectormap.route.RouteLineSegment
-import com.kakao.vectormap.route.RouteLineStyle
-import com.kakao.vectormap.route.RouteLineStyles
-import com.kakao.vectormap.route.RouteLineStylesSet
-import com.yong.km_assignment.R
-import com.yong.km_assignment.data.model.RouteDetail
 import com.yong.km_assignment.data.model.RouteInfo
 import com.yong.km_assignment.ui.common.StatusTextView
 import com.yong.km_assignment.ui.theme.KakaoMobilityAssignmentTheme
-import com.yong.km_assignment.ui.theme.*
-import com.yong.km_assignment.util.LogUtil.LogD
 import com.yong.km_assignment.util.NetworkUtil
 
 class MapviewActivity: ComponentActivity() {
@@ -85,10 +62,10 @@ class MapviewActivity: ComponentActivity() {
                                 .fillMaxSize()
                                 .padding(innerPadding)
                         ) {
-                            if (it == true) {
+                            if (it) {
                                 val routeDetail = viewModel.routeDetail
                                 if (routeDetail != null) {
-                                    KakaoMapView(
+                                    MapviewKakaoMap(
                                         routeDetail = routeDetail,
                                         modifier = Modifier
                                     )
@@ -143,113 +120,5 @@ fun RouteInfoView(
                 msg = "경로 시간/거리 정보를 불러오지 못했습니다.",
             )
         }
-    }
-}
-
-@Composable
-fun KakaoMapView(
-    routeDetail: List<RouteDetail>,
-    modifier: Modifier = Modifier
-) {
-    AndroidView(
-        factory = { context ->
-            val mapView = MapView(context)
-            mapView.start(
-                object: MapLifeCycleCallback() {
-                    override fun onMapDestroy() {
-                        LogD("KakaoMapView", "Map Destroyed")
-                    }
-
-                    override fun onMapError(error: Exception) {
-                        LogD("KakaoMapView", "Map Error: ${error.message}")
-                    }
-                },
-                object: KakaoMapReadyCallback() {
-                    override fun onMapReady(kakaoMap: KakaoMap) {
-                        LogD("KakaoMapView", "Map is Ready")
-
-                        addRouteLabelView(kakaoMap, routeDetail)
-                        setKakaoMapCameraView(kakaoMap, routeDetail)
-                        setKakaoMapRouteLine(kakaoMap, routeDetail)
-                    }
-                }
-            )
-
-            return@AndroidView mapView
-        },
-        modifier = modifier.fillMaxSize()
-    )
-}
-
-fun addRouteLabelView(
-    kakaoMap: KakaoMap,
-    routeDetail: List<RouteDetail>
-) {
-    kakaoMap.labelManager!!.layer?.let {
-        val latlngFrom = routeDetail.first().routePointList.split(" ")[0].split(",")
-        val labelStylesFrom = kakaoMap.labelManager!!.addLabelStyles(LabelStyles.from(
-            LabelStyle.from(R.drawable.ic_marker_from)))
-        val labelOptionsFrom = LabelOptions.from(LatLng.from(latlngFrom[1].toDouble(), latlngFrom[0].toDouble())).let { option ->
-            option.setStyles(labelStylesFrom)
-            option.setTexts(LabelTextBuilder().setTexts("출발"))
-        }
-        it.addLabel(labelOptionsFrom)
-
-        val latlngTo = routeDetail.last().routePointList.split(" ")[0].split(",")
-        val labelStylesTo = kakaoMap.labelManager!!.addLabelStyles(LabelStyles.from(
-            LabelStyle.from(R.drawable.ic_marker_to)))
-        val labelOptionsTo = LabelOptions.from(LatLng.from(latlngTo[1].toDouble(), latlngTo[0].toDouble())).let { option ->
-            option.setStyles(labelStylesTo)
-            option.setTexts(LabelTextBuilder().setTexts("도착"))
-        }
-        it.addLabel(labelOptionsTo)
-    }
-}
-
-fun setKakaoMapCameraView(
-    kakaoMap: KakaoMap,
-    routeDetail: List<RouteDetail>
-) {
-    val latlngFrom = routeDetail.first().routePointList.split(" ")[0].split(",")
-    val latlngTo = routeDetail.last().routePointList.split(" ")[0].split(",")
-    val latlngCenter = LatLng.from((latlngFrom[1].toDouble() + latlngTo[1].toDouble()) / 2, (latlngFrom[0].toDouble() + latlngTo[0].toDouble()) / 2)
-    kakaoMap.moveCamera(zoomTo(10))
-    kakaoMap.moveCamera(newCenterPosition(latlngCenter), CameraAnimation.from(500, true, true))
-}
-
-fun setKakaoMapRouteLine(
-    kakaoMap: KakaoMap,
-    routeDetail: List<RouteDetail>
-) {
-    val mapLayer = kakaoMap.routeLineManager!!.layer
-    val routeStyleNormal: RouteLineStylesSet = RouteLineStylesSet.from(
-        "RouteLine",
-        RouteLineStyles.from(RouteLineStyle.from(15f, RouteUnknown.toArgb())),
-        RouteLineStyles.from(RouteLineStyle.from(15f, RouteJam.toArgb())),
-        RouteLineStyles.from(RouteLineStyle.from(15f, RouteDelay.toArgb())),
-        RouteLineStyles.from(RouteLineStyle.from(15f, RouteSlow.toArgb())),
-        RouteLineStyles.from(RouteLineStyle.from(15f, RouteNormal.toArgb())),
-        RouteLineStyles.from(RouteLineStyle.from(15f, RouteBlock.toArgb()))
-    )
-
-    routeDetail.forEach { routeDetailItem ->
-        val routePointList = routeDetailItem.routePointList.split(" ").map {
-            val pointPair = it.split(",")
-            LatLng.from(pointPair[1].toDouble(), pointPair[0].toDouble())
-        }
-
-        val mapRouteSegment = RouteLineSegment.from(routePointList)
-            .setStyles(
-                when (routeDetailItem.routeTraffic) {
-                    "UNKNOWN" -> routeStyleNormal.getStyles(0)
-                    "JAM" -> routeStyleNormal.getStyles(1)
-                    "DELAY" -> routeStyleNormal.getStyles(2)
-                    "SLOW" -> routeStyleNormal.getStyles(3)
-                    "NORMAL" -> routeStyleNormal.getStyles(4)
-                    else -> routeStyleNormal.getStyles(5)
-                }
-            )
-        val options = RouteLineOptions.from(mapRouteSegment)
-        mapLayer.addRouteLine(options)
     }
 }
