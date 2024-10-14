8a1ea31d5806d2b13ee99a3fdc8010d44559a9b4
yymin1022 <yymin1022@gmail.com>
2024-10-14 17:15:05 +0900

    Update MapviewUiState : Cleaned UP

--- app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewUiState.kt
+++ app/src/main/java/com/yong/km_assignment/ui/mapview/MapviewUiState.kt
@@ -4,7 +4,7 @@ import com.yong.km_assignment.data.model.RouteDetail
 import com.yong.km_assignment.data.model.RouteInfo
 
 sealed class UIState {
-    object Loading : UIState()
-    data class Success(val routeDetail: List<RouteDetail>, val routeInfo: RouteInfo?) : UIState()
-    data class Error(val errorCode: Int, val errorMessage: String) : UIState()
+    data object Loading: UIState()
+    data class Success(val routeDetail: List<RouteDetail>, val routeInfo: RouteInfo?): UIState()
+    data class Error(val errorCode: Int, val errorMessage: String): UIState()
 }
