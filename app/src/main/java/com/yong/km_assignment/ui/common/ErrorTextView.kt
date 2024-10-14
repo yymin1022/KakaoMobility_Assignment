5e3d5246f8699241622df3bc574ea75b0aad4a47
yymin1022 <yymin1022@gmail.com>
2024-10-14 15:31:02 +0900

    Update ErrorTextView : Added Composable Widget

--- app/src/main/java/com/yong/km_assignment/ui/common/ErrorTextView.kt
+++ app/src/main/java/com/yong/km_assignment/ui/common/ErrorTextView.kt
@@ -1,1 +1,15 @@
 package com.yong.km_assignment.ui.common
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ErrorTextView(modifier: Modifier, msg: String) {
+    Text(
+        modifier = modifier,
+        fontSize = 20.sp,
+        text = msg
+    )
+}
